8ed206b3a9ac29c54c0e8da5165d363aecba251f
Peter Obi <52758657+Peter-obi@users.noreply.github.com>
2024-06-12 17:11:48 -0700

    Update gcn.py

--- GCN_protein_conformations/model/gcn.py
+++ GCN_protein_conformations/model/gcn.py
@@ -1,17 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, global_mean_pool
 
 class GCN(nn.Module):
-    def __init__(self, num_node_features, num_classes):
+    def __init__(self, input_dim, hidden_channels):
         super(GCN, self).__init__()
-        self.conv1 = GCNConv(num_node_features, 16)
-        self.conv2 = GCNConv(16, num_classes)
+        torch.manual_seed(42)
+        self.conv1 = GCNConv(input_dim, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.lin = nn.Linear(hidden_channels, 3)
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-        x = self.conv1(x, edge_index)
+    def forward(self, x, edge_index, batch, edge_weight=None):
+        x = self.conv1(x, edge_index, edge_weight)
         x = F.relu(x)
-        x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)
+        x = self.conv2(x, edge_index, edge_weight)
+        x = F.relu(x)
+        x = self.conv3(x, edge_index, edge_weight)
+        x = global_mean_pool(x, batch)
+        x = self.lin(x)
+        return F.log_softmax(x, dim=-1)
