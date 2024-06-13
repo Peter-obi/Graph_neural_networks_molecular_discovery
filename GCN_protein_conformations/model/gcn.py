acf2d62a07209e85ad14360697f3c0ae0f6c21c4
Peter Obi <52758657+Peter-obi@users.noreply.github.com>
2024-06-12 18:40:26 -0700

    Update gcn.py

--- GCN_protein_conformations/model/gcn.py
+++ GCN_protein_conformations/model/gcn.py
@@ -4,7 +4,17 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, global_mean_pool
 
 class GCN(nn.Module):
+    """
+    Graph Convolutional Network (GCN) model.
+    """
     def __init__(self, input_dim, hidden_channels):
+        """
+        Initialize the GCN model.
+
+        Args:
+            input_dim (int): Dimension of the input features.
+            hidden_channels (int): Number of hidden channels.
+        """
         super(GCN, self).__init__()
         torch.manual_seed(42)
         self.conv1 = GCNConv(input_dim, hidden_channels)
@@ -13,6 +23,18 @@ class GCN(nn.Module):
         self.lin = nn.Linear(hidden_channels, 3)
 
     def forward(self, x, edge_index, batch, edge_weight=None):
+        """
+        Forward pass of the GCN model.
+
+        Args:
+            x (torch.Tensor): Node feature matrix.
+            edge_index (torch.Tensor): Edge index tensor.
+            batch (torch.Tensor): Batch tensor.
+            edge_weight (torch.Tensor, optional): Edge weight tensor. Defaults to None.
+
+        Returns:
+            torch.Tensor: Output of the GCN model.
+        """
         x = self.conv1(x, edge_index, edge_weight)
         x = F.relu(x)
         x = self.conv2(x, edge_index, edge_weight)
